87b70b9a52202180cf703f7bbd2b45a13ae1c9c6
loogark <ragoolkrishnan.ram@gmail.com>
2024-07-15 00:43:42 +0200

    chore: Update @notice-org/renderer-helper to version 1.4.1 and allow user to add custom head code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
+import { CustomCodeInjector } from '@/components/CustomCodeInjector'
 import { Navbar } from '@/components/Navbar'
 import { NoticeLabel } from '@/components/NoticeLabel'
 import { Providers } from '@/providers'
 import { API, extractProjectID } from '@/tools/api'
 import { headers } from 'next/headers'
+import Script from 'next/script'
 
 export default async function RootLayout({
 	children,
@@ -10,16 +12,23 @@ export default async function RootLayout({
 	children: React.ReactNode
 }>) {
 	const projectData = await getProjectData()
-	const { hideCreatedWithNotice } = projectData?.project || {}
+	const { hideCreatedWithNotice, headCode } = projectData?.project || {}
 
 	return (
 		<html lang="en">
+			<head>
+				<Script id="custom-code-script" strategy="afterInteractive">
+					{`window.__CUSTOM_CODE__ = ${JSON.stringify(headCode)};`}
+				</Script>
+			</head>
+
 			<body>
 				<Providers>
 					<Navbar meta={projectData?.metadata} />
 					{children}
 					<NoticeLabel shouldHide={hideCreatedWithNotice} />
 				</Providers>
+				<CustomCodeInjector />
 			</body>
 		</html>
 	)
@@ -31,7 +40,7 @@ async function getProjectData() {
 	if (!projectId) return null
 
 	try {
-		const { data } = await API.get(`/projects/${projectId}`)
+		const { data } = await API.get(`/projects/47609ad8-52d8-4713-a6e3-1fbd233935b0`)
 		return data
 	} catch (_) {
 		return null
